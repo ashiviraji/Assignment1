c8a8af097eb243f904f6dec86f36793f2acc4055
ashiviraji <46102737+ashiviraji@users.noreply.github.com>
2020-05-23 14:23:33 +0530

    Second commit

---
+++ ScalaQuestions/src/volume.scala
@@ -0,0 +1,10 @@
+
+
+object volume {
+  def main(args: Array[String]){
+    var radius:Double=5
+    var vol:Double=0.0
+    vol=((4f/3)*(22f/7)*radius*radius*radius)
+    println("The volume is:"+vol)
+  }
+}
